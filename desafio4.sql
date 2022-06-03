4b8380826b9f5da35b52ebe956d8eb2aeff5beab
Igor Leal <igorhenriqueleal3@gmail.com>
2022-06-03 18:38:03 -0300

    fix: Correcao no alias sobre condicao do usuario do requisito 4

--- desafio4.sql
+++ desafio4.sql
@@ -2,7 +2,7 @@ SELECT
   users.user_name AS usuario,
   IF (MAX(YEAR(historics.reproduction_date)) < 2021, 
     'Usario inativo',
-    'Usuario ativo') AS total_minutos
+    'Usuario ativo') AS condicao_usuario
 FROM SpotifyClone.users AS users
   INNER JOIN SpotifyClone.historics AS historics ON historics.user_id = users.user_id
 GROUP BY usuario;
