03033431b309b1f608ef462321fa17ac61c23650
Igor Leal <igorhenriqueleal3@gmail.com>
2022-06-03 20:40:43 -0300

    fix: Adicionado acento na palavra Usuário na query do Desafio 4

--- desafio4.sql
+++ desafio4.sql
@@ -1,7 +1,7 @@
 SELECT 
-  users.user_name AS 'usuario',
-  IF(YEAR(MAX(historics.reproduction_date)) = 2021, 'Usario ativo', 'Usuario inativo') AS 'condicao_usuario'
+  users.user_name AS usuario,
+  IF(MAX(YEAR(historics.reproduction_date)) = 2021, 'Usuário ativo', 'Usuário inativo') AS 'condicao_usuario'
 FROM SpotifyClone.users AS users
   INNER JOIN SpotifyClone.historics AS historics ON  users.user_id = historics.user_id
-GROUP BY users.user_id
-ORDER BY usuario;
+GROUP BY users.user_name
+ORDER BY users.user_name;
